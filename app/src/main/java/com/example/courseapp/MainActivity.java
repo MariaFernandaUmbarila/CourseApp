991b11eb4751a0363f44cf489fe19d557c9ec9b1
MariaFernandaUmbarila <fernandaumbarila@gmail.com>
2024-07-02 16:57:36 -0500

    Modifications on string resource

--- app/src/main/java/com/example/courseapp/MainActivity.java
+++ app/src/main/java/com/example/courseapp/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.courseapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    //Logical grid for the game
     String[][] board = new String[3][3];
+    //X for true, O for false
     boolean turn = false;
     String player = "", winner = "";
+    //Counters for the quantity of Xs and Os
     int xCounter = 0, oCounter = 0;
     Button startButton;
 
@@ -23,21 +22,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startButton = (Button) findViewById(R.id.startButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startBoard();
-            }
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(v -> {
+            startBoard();
+            startButtons();
         });
     }
 
+    //Fill the board matrix with _
     private void startBoard(){
-        for (int row = 0; row <= 2; row++)
-            for (int col = 0; col <= 2; col++)
+        for (int row = 0; row < 3; row++)
+            for (int col = 0; col < 3; col++)
                 board[row][col] = "_";
     }
 
+    //Change the player in each turn
     private void playTurn(int row, int column){
         turn = !turn;
         player = turn ? "X" : "O";
@@ -49,13 +48,78 @@ public class MainActivity extends AppCompatActivity {
         for (int row = 0; row < 3; row++){
             for (int col = 0; col < 3; col++){
 
+                //Look for every button named after its position on the matrix
                 buttonId = getResources().getIdentifier("button" + row + col, "id", getPackageName());
-                Button selectedButton = (Button) findViewById(buttonId);
+                Button selectedButton = findViewById(buttonId);
                 selectedButton.setText("");
                 selectedButton.setEnabled(true);
 
-                startButton.setText("Start Game");
+                startButton.setText(R.string.startGameButton);
+                turn = false;
+                winner = "";
+                xCounter = 0;
+                oCounter = 0;
+
+                final int r = row;
+                final int c = col;
+
+                selectedButton.setOnClickListener(v -> {
+                    if (selectedButton.getText().toString().isEmpty() && winner.isEmpty()){
+                        playTurn(r, c);
+                        selectedButton.setText(player);
+                        startButton.setText(R.string.restartGameButton);
+                        lookForWinner();
+                    }
+                });
             }
         }
+    }
+
+    private void lookForWinner(){
+
+        for (int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++)
+                checkMove(row, col);
+            if(!winner.isEmpty()) break;
+            xCounter = 0;
+            oCounter = 0;
+        }
+
+        if (winner.isEmpty()){
+            for (int col = 0; col < 3; col++){
+                for (int row = 0; row < 3; row++)
+                    checkMove(row, col);
+                if(!winner.isEmpty()) break;
+                xCounter = 0;
+                oCounter = 0;
+            }
+        }
+
+        if(winner.isEmpty()){
+            int row;
+            for (int col = 0; col < 3; col++){
+                row = col;
+                checkMove(row, col);
+            }
+            xCounter = 0;
+            oCounter = 0;
+        }
+
+        if(!winner.isEmpty()){
+            Toast.makeText(getApplicationContext(), winner + " wins", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void checkMove(int row, int col){
+        String value = board[row][col];
+
+        if(!value.isEmpty()){
+            if(value.equals("X")) xCounter++;
+            if(value.equals("O")) oCounter++;
+        }
+
+        if (xCounter == 3) winner = "X";
+        if (oCounter == 3) winner = "O";
     }
 }
